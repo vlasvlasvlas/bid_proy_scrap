460c84cb6868a53a91e34ca281117b5113557ee7
vlasvlasvlas <vlasvlasvlas@gmail.com>
2022-03-25 12:59:50 -0300

    up

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # bid_proy_scrap
+
+## Objetivo
+El objetivo del script es la generación en formato deutilizable xlsx/csv de la información de proyectos y operaciones de prestamos BID por país.
+
+Para esto se utiliza un proceso de scrapeo de datos html cuyo origen es el listado de proyectos BID por país.
+
+## Requerimientos
+- Python 3
+- Extensiones Pandas, BeautifulSoup
+
+
+## Proceso
+- Toma listado de id de proyectos de proyectos_lista.csv
+- Itera por cada fila, recreando la url del proyecto. Por cada url:
+	- utiliza beautifulsoup para extración scrap de los tags de project-field-title y project-field-data del sitio
+	- almacena pares es diccionario
+- Pasa total de resultados a un pandas DataFrame y exporta resultado en xlsx/csv
+
+## To-Do
+- Traer url xlsx de listado de proyectos, pensando en uno o varios países (latam?)
+
---
+++ scrap_proyectos.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+#url = 'https://www.iadb.org/es/project-search-xls?country=PE&sector=&status=&query='
+#urls = ['https://www.iadb.org/es/project/PE-L1271']
+
+dfurls = pd.read_csv('lista.csv') #to-do: get lista from xlsx web
+urls = dfurls['proyectos']
+dall  = []
+for idx, url in enumerate(urls):
+    print(str(idx+1)+"/"+str(dfurls.shape[0])+' '+url)
+    page = requests.get('https://www.iadb.org/es/project/'+str(url))
+    drow = dict()
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find(id="main")
+        proyecto = soup.find_all("div", {"class": "project-detail"})
+        for proy in proyecto:
+            prcols = []
+            prdata = []
+            pritems = soup.find_all("div", {"class": "project-field"})
+            for pritem in pritems:
+                try:
+                    title=str(pritem.find('div', class_='project-field-title').string).strip()
+                except:
+                    title='-'
+                try:
+                    data=str(pritem.find('span', class_='project-field-data').string).strip()
+                except:
+                    data='-'
+                drow[title] = data
+    dall.append(drow)
+dfall = pd.DataFrame(dall)
+dfall.to_excel('proyectos_lista_output.xlsx')
